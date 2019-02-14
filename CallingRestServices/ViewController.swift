1bb1c2e34baff0fe9a5b2d1913834017b0111296
Sme Angeles <Esmeralda.angelesm@hotmail.com>
2019-02-13 22:22:39 -0600

    Initial Commit

---
+++ CallingRestServices/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CallingRestServices
+//
+//  Created by Esmeralda Angeles on 2/13/19.
+//  Copyright © 2019 Sme.Angeles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
